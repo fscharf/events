6f331a40493e5b53fa9091fd20a014be1498cda1
ribeiro1 <ribeiro1samuka2.0@gmail.com>
2020-06-02 12:40:22 -0300

    Update EventsController.cs

--- Events/Controllers/EventsController.cs
+++ Events/Controllers/EventsController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//samuel teste commit okay
 //Gabriel Teste commit okas
 namespace Events.Controllers
 {
     public class EventsController : Controller
     {
         // exemplo git
+        // teste okay
         public ActionResult Index()
         {
             return View();
